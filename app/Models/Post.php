73955609cf9c92eca623d479c78b5013b3a4d8ab
daisuke sasaki <gerund25495641@gmail.com>
2020-10-14 20:57:21 +0900

    controllerまで作成済み

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $post = 'laraveldb';
+    protected $fillable = ['title', 'body'];
+}
